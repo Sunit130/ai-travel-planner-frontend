91ffc79c774e8bf1d13a5a4bca64c92671658d03
sunit-iquanti <patwardhansunit@gmail.com>
2025-02-14 23:21:30 +0530

    TravelTales | updated travel types

--- src/app/const.ts
+++ src/app/const.ts
@@ -1,4 +1,4 @@
 export const TRIPS_TYPE = [
   "Adventure", "Exploration", "Nature", "Culture", "Historical",  
-  "Food", "Hiking", "Wildlife", "Sightseeing", "Beach", "Romantic", "Budget"
+  "Food", "Hiking", "Wildlife", "Sightseeing", "Beach", "Romantic", "Budget", "Luxury"
 ];
